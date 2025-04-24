290d2e841dfb65b4b384380ae37e74a8a971ca29
Matmattb <mbeurotnury@gmail.com>
2025-04-24 16:19:35 +0200

    Tenter deploiement

--- server/server.js
+++ server/server.js
@@ -4,5 +4,6 @@ const routes = require('./src/routes');
 // Utiliser les routes
 app.use('/api', routes);
 
+module.exports = app;
 // Démarrer le serveur
-app.listen(3000, () => console.log('Server running on port 3000')); 
+//app.listen(3000, () => console.log('Server running on port 3000')); 
